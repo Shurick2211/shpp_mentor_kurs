3d16eb35a7677e62fde16314406a95649c4ba5f3
Alexandr Nimko <shurick2211@gmail.com>
2022-10-24 19:25:04 +0300

    3

---
+++ src/onimko/loger/my-loger/src/main/java/onimko/App.java
@@ -0,0 +1,16 @@
+package onimko;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ){
+        log.info("Hello mentor course!");
+
+    }
+}
