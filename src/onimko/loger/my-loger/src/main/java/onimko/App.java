882ebdfb0f30cfb45986645ed7ddbde401bae160
Alexandr Nimko <shurick2211@gmail.com>
2022-10-25 11:31:41 +0300

    +resources

--- src/onimko/loger/my-loger/src/main/java/onimko/App.java
+++ src/onimko/loger/my-loger/src/main/java/onimko/App.java
@@ -1,5 +1,10 @@
 package onimko;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +16,30 @@ public class App {
     static final Logger log = LoggerFactory.getLogger(App.class);
     public static void main( String[] args ){
         log.info("Hello mentor course!");
+        //Properties file
+        Properties appProps = new Properties();
+        InputStream resPath = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("my-properties.properties");
+        try {
+            appProps.load(resPath);
+        } catch (IOException e) {
+            log.error("File .properties not found!");
+            throw new RuntimeException(e);
+        }
+        log.info(appProps.getProperty("message"));
+
+        //xml-file
+        InputStream rootPath = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("hello.xml");
+        Properties xmlProps = new Properties();
+        try {
+            xmlProps.loadFromXML(rootPath);
+        } catch (IOException e) {
+            log.error("File .xml not found!");
+            throw new RuntimeException(e);
+        }
+        log.info(xmlProps.getProperty("hello"));
 
     }
+
 }
